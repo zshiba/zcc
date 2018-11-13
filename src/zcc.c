54ef0678a73c5c15693b9ef49518d2968f497e4b
zshiba <zshiba@users.noreply.github.com>
2018-11-13 13:53:44 -0500

    Update zcc.c to produce an assembly returning 14

--- src/zcc.c
+++ src/zcc.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
 #include "zcc.h"
 
 int main(int argc, char* argv[]){
+
+  printf(".intel_syntax noprefix\n");
+  printf(".global _main\n");
+  printf("_main:\n");
+  printf("  mov rax, 14\n");
+  printf("  ret\n");
+
   return 0;
 }
