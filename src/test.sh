33a60513c6ba7413cdd48a265defeb700ff7f440
zshiba <zshiba@users.noreply.github.com>
2019-01-15 16:15:29 -0500

    Add addition and subtraction

---
+++ src/test.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+try() {
+  expected="$1"
+  input="$2"
+
+  ./zcc "${input}" > temp.s
+  gcc -o temp temp.s
+  ./temp
+  actual="$?"
+
+  if [[ "${actual}" == "${expected}" ]]; then
+    echo "${input} => ${actual}"
+  else
+    echo "${expected} rexpected, but got ${actual}"
+  fi
+}
+
+echo "=== Test Start ==="
+try 21 '5+20-4'
+echo "=== Test End ==="
--- src/zcc.c
+++ src/zcc.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "zcc.h"
 
 int main(int argc, char* argv[]){
+  if(argc != 2){
+    fprintf(stderr, "Wrong number of arguments\n");
+    return 1;
+  }else{
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global _main\n");
-  printf("_main:\n");
-  printf("  mov rax, 14\n");
-  printf("  ret\n");
+    printf(".intel_syntax noprefix\n");
+    printf(".global _main\n");
+    printf("_main:\n");
 
-  return 0;
+    char* p = argv[1];
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    while((*p) != '\0'){
+      if((*p) == '+'){
+        ++p;
+        printf("  add rax, %ld\n", strtol(p, &p, 10));
+      }else if((*p) == '-'){
+        ++p;
+        printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      }else{
+        fprintf(stderr, "Unexpected charactor: '%c'\n", (*p));
+        return 1;
+      }
+    }
+    printf("  ret\n");
+
+    return 0;
+  }
 }
