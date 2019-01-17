59942c29ef36e97627c1a4070e02f8f100fad6a1
zshiba <zshiba@users.noreply.github.com>
2019-01-17 15:00:46 -0500

    Add Vector for tokens

--- src/zcc.c
+++ src/zcc.c
@@ -8,6 +8,29 @@ void error(char* format, char* input){
   exit(1);
 }
 
+typedef struct{
+  void** data;
+  int capacity;
+  int length;
+}Vector;
+
+Vector* new_vector(){
+  Vector* v = malloc(sizeof(Vector));
+  v->capacity = 16;
+  v->data = malloc(sizeof(void*) * v->capacity);
+  v->length = 0;
+  return v;
+}
+
+void vector_push(Vector* v, void* element){
+  if(v->capacity == v->length){
+    v->capacity *= 2;
+    v->data = realloc(v->data, sizeof(void*) * v->capacity);
+  }
+  v->data[v->length] = element;
+  v->length = v->length + 1;
+}
+
 enum{
   TOKEN_NUMBER = 256, //to avoid these overlapping ascii table
   TOKEN_EOF
@@ -19,33 +42,35 @@ typedef struct{
   char* input;
 }Token;
 
-Token tokens[100]; //ad-hoc, fixed size just for now
+Token* new_token(int type, int value, char* input){
+  Token* token = malloc(sizeof(Token));
+  token->type = type;
+  token->value = value;
+  token->input = input;
+  return token;
+}
+
+Vector* tokens = NULL; //ad-hoc, still in global
 int position = 0;  //ad-hoc, the index of current charactor in the given source
 
 void tokenize(char* p){
-  int i = 0;
   while((*p) != '\0'){
     if(isspace((*p))){
       ++p;
     }else if((*p) == '+' || (*p) == '-' || (*p) == '*' || (*p) == '/' || (*p) == '(' || (*p) == ')'){
-      tokens[i].type = (*p);
-      tokens[i].value = 0; //ad-hoc, redundant
-      tokens[i].input = p;
-      ++i;
+      Token* token = new_token((*p), 0, p);
+      vector_push(tokens, token);
       ++p;
     }else if(isdigit((*p))){
-      tokens[i].type = TOKEN_NUMBER;
-      tokens[i].value = strtol(p, &p, 10);
-      tokens[i].input = p;
-      ++i;
+      Token* token = new_token(TOKEN_NUMBER, strtol(p, &p, 10), p);
+      vector_push(tokens, token);
     }else{
       fprintf(stderr, "(Error) Failure to tokenize: '%s'\n", p);
       exit(1);
     }
   }
-  tokens[i].type = TOKEN_EOF;
-  tokens[i].value = 0; //ad-hoc, redundant
-  tokens[i].input = p;
+  Token* token = new_token(TOKEN_EOF, 0, p);
+  vector_push(tokens, token);
 }
 
 enum{
@@ -69,7 +94,7 @@ Node* new_node(int type, int value, Node* lhs, Node* rhs){
 }
 
 int consume(int type){
-  if(tokens[position].type == type){
+  if(((Token*)tokens->data[position])->type == type){
     ++position;
     return 1;
   }else{
@@ -85,13 +110,13 @@ Node* term(){
     if(consume(')'))
       return node;
     else
-      error("(Error) No closing parenthesis: %s\n", tokens[position].input);
-  }else if(tokens[position].type == TOKEN_NUMBER){
-    Node* newbie = new_node(NODE_NUMBER, tokens[position].value, NULL, NULL);
+      error("(Error) No closing parenthesis: %s\n", ((Token*)tokens->data[position])->input);
+  }else if(((Token*)tokens->data[position])->type == TOKEN_NUMBER){
+    Node* newbie = new_node(NODE_NUMBER, ((Token*)tokens->data[position])->value, NULL, NULL);
     ++position;
     return newbie;
   }else{
-    error("(Error) Unknown token: %s\n", tokens[position].input);
+    error("(Error) Unknown token: %s\n", ((Token*)tokens->data[position])->input);
   }
 }
 
@@ -167,6 +192,7 @@ int main(int argc, char* argv[]){
     return 1;
   }else{
 
+    tokens = new_vector();
     tokenize(argv[1]);
 //for(int i = 0; i < 100; i++)
 //  fprintf(stderr, "%d %d\n", tokens[i].type, tokens[i].value);
